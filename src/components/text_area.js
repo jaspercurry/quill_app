f3b36f17e353e820b4a771bb76f12952e584fc50
jaspercurry <jaspercurry@gmail.com>
2016-11-06 23:41:40 -0500

    adjusted rough logic

---
+++ src/components/text_area.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {connect} from 'react-redux'
+import * as actions from '../actions'
+import { bindActionCreators } from 'redux'
+
+
+
+
+class TextArea extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {value: ''}
+    this.handleChange = this.handleChange.bind(this)
+    this.fetchCompaniesAction = this.fetchCompaniesAction.bind(this)
+  }
+
+  handleChange(e) {
+    e.preventDefault()
+    var tickers = [...this.props.companies, this.refs.ticker.value.toUpperCase()]
+    this.refs.ticker.value = ""
+    this.props.actions.setCompanies(tickers)
+    this.fetchCompaniesAction(tickers) // had issues with the setCompanies action - had to pass in tickers object to fetchCompaniesAction instead of calling on this.props.companies
+  }
+
+  fetchSubmitText(tickers) {
+    if (tickers.length > 0) {
+      var formattedTickers = tickers.join()
+      this.props.actions.fetchData(formattedTickers)
+    }
+    setTimeout(() =>this.fetchCompaniesAction(this.props.companies), 60000); // refreshes stock data every minute - there is a bug that causes too many requests to me sent. Need to refactor.
+  }
+
+
+  render(){
+    return (
+    <div className='col-md-4'>
+      <form onSubmit={this.handleChange}>
+      <input type='text' ref="ticker" placeholder='Ticker Symbol' />
+      </form>
+  </div>)
+  }
+}
+
+function mapStateToProps(state) {
+  return (
+    {companies: state.companies}
+  )
+}
+
+function mapDispatchToProps(dispatch) {
+  return { actions: bindActionCreators(actions, dispatch) }
+}
+
+const componentCreator = connect(mapStateToProps, mapDispatchToProps)
+export default componentCreator(StockSearch);
